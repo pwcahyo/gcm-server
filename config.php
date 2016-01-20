5de9949e908fad57a63ebfe8724c9f24f41e6dfa
insomniart <pwcahyo@gmail.com>
2016-01-20 13:35:50 +0700

    tambahan port tapi tidak dipake

--- config.php
+++ config.php
@@ -3,6 +3,7 @@
  * DB config
  */
 define("DB_HOST", "localhost");
+define("DB_PORT", 9090);
 define("DB_USER", "root");
 define("DB_PASSWORD", "toor");
 define("DB_DATABASE", "db_gcm_demo");
--- db_connect.php
+++ db_connect.php
@@ -13,7 +13,7 @@ class DB_Connect {
     public function connect() {
         require_once 'config.php';
         
-        $conn = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);        
+        $conn = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD,DB_PORT);        
         mysql_select_db(DB_DATABASE); 
         
         return $conn;
